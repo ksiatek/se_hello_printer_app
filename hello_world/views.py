bb45c8ea23745d9d7cb066e5871a80ee43896a4b
ksiatek <ksiatek@users.noreply.github.com>
2020-02-23 13:04:26 +0100

    Zmiany w testach json i xml

--- hello_world/views.py
+++ hello_world/views.py
@@ -10,9 +10,13 @@ msg = "Hello World!"
 @app.route('/')
 def index():
     output = request.args.get('output')
+    name = request.args.get('name')
     if not output:
         output = PLAIN
-    return get_formatted(msg, moje_imie,
+    if not name:
+        name = moje_imie
+
+    return get_formatted(msg, name,
                          output.lower())
 
 
